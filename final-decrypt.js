7494399da75b459c03d9259b8cd7d2b25fc76479
shah527 <54545512+shah527@users.noreply.github.com>
2022-07-13 10:44:54 -0400

    Create final-decrypt.js

---
+++ final-decrypt.js
@@ -0,0 +1,4 @@
+//time taken ~17ms
+let c = require('crypto');
+let d = c.createDecipheriv('aes-128-cbc', 'mysecretpassword', Buffer.from(JSON.parse(Buffer.from(process.argv[2], 'base64').toString()).data[1], 'base64').toString());
+console.log(d.update(JSON.parse(Buffer.from(process.argv[2], 'base64').toString()).data[0], 'base64', 'utf8') + d.final('utf8'));
