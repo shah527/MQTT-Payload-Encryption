bbc90a0eeccaa5a02f93e736d36eb231ab880c7b
shah527 <54545512+shah527@users.noreply.github.com>
2022-07-14 09:31:55 -0400

    Update time.py

--- time.py
+++ time.py
@@ -1,5 +1,6 @@
 import subprocess
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 
 numbers = []
 trials = []
@@ -16,16 +17,21 @@ def Average(lst):
     return sum(lst) / len(lst)
 
 
-numbers = [int(float(i)) for i in numbers]
+numbers = [float(i) / 1000000 for i in numbers]
 print(numbers)
 print("average " + str(Average(numbers)))
 
-plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.figsize"] = [6.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
+
+fig, ax = plt.subplots()
+ax.scatter(trials, numbers, color="purple", label='Execution Time', s=2)
+ax.yaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter("{x:.4f}"))
 plt.title("Performance Test")
-plt.plot(trials, numbers, color="lightblue", label='Execution Time')
 plt.plot(trials, [sum(numbers) / len(numbers) for i in trials], color="green", label='Average', linestyle='--')
 plt.xlabel("Order of Trials")
+plt.ylabel("Time (" + u"\u03bcs" + ")")
 plt.ylabel("Time (ms)")
+plt.ylim((12, 14))
 plt.legend(loc='upper left')
 plt.show()
