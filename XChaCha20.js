25b5a7073a516b00a8e2dafa0bb9be3f9c7bf04e
shah527 <54545512+shah527@users.noreply.github.com>
2022-07-20 15:46:08 -0400

    Update and rename chachabackup.js to XChaCha20.js

---
+++ XChaCha20.js
@@ -0,0 +1,4 @@
+let x = process.hrtime.bigint();
+const { decrypt, getXchaCha20Poly1305Cipher } = require('@xchacha20-192bit/core')
+console.log(Buffer.from(decrypt(getXchaCha20Poly1305Cipher(Buffer.from('14wJ5sfw+TXBHmmWk4RU9AUixM46TWxr1wqRvcenCdc=', 'base64')), Buffer.from(JSON.parse(Buffer.from(process.argv[2], 'base64').toString()).data[1], 'base64'), Buffer.from(JSON.parse(Buffer.from(process.argv[2], 'base64').toString()).data[0], 'base64'))).toString())
+console.log(process.hrtime.bigint()-x)
