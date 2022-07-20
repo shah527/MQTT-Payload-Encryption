be911f70dde3a6651e809b6d53e5b3e026fc2314
shah527 <54545512+shah527@users.noreply.github.com>
2022-07-20 15:46:29 -0400

    Delete xchacha20-decrypt.js

--- xchacha20-decrypt.js
+++
@@ -1,5 +0,0 @@
-//eyJkYXRhIjpbImN4WHU3Ym92bEo3dEZHbEY2aUpHUytpenp4RmVxaWV4Z1BVK0pVaXAiLCJCY3JFeDJpUVA3dVZFRHlCNFRpSk1pckRTM05QWnJmdyJdfQ==
-let x = process.hrtime.bigint();
-const { decrypt, getXchaCha20Poly1305Cipher } = require('@xchacha20-192bit/core')
-console.log(Buffer.from(decrypt(getXchaCha20Poly1305Cipher(Buffer.from('14wJ5sfw+TXBHmmWk4RU9AUixM46TWxr1wqRvcenCdc=', 'base64')), Buffer.from(JSON.parse(Buffer.from(process.argv[2], 'base64').toString()).data[1], 'base64'), Buffer.from(JSON.parse(Buffer.from(process.argv[2], 'base64').toString()).data[0], 'base64'))).toString())
-console.log(process.hrtime.bigint()-x)
